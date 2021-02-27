313eb1f3cae7ea14d0ce9c21994d9a667c1b14c6
Yvana <yvana.lopes@grupoboticario.com.br>
2021-02-27 16:33:16 -0300

    atualizando algumas correções

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -3,16 +3,16 @@ import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
-    const { currentExpierence, experienceToNextLevel } = useContext(ChallengesContext)
+    const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext)
 
-    const percentToNextLevel = Math.round(currentExpierence * 100) / experienceToNextLevel
+    const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel
     return (
         <header className={styles.experienceBar}>
             <span>0px</span>
             <div>
                 <div style={{ width: `${percentToNextLevel}%` }} />
                 <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
-                    {currentExpierence} xp
+                    {currentExperience} pt
                 </span>
             </div>
             <span>{experienceToNextLevel} px</span>
